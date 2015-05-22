5c5c579014fc9bbed5e5f4c363d5a9c3af0ce29c
Wille Marcel <wille@wille.blog.br>
2015-05-22 10:52:35 -0300

    rename class to SentinelAPI and add **keywords parameter to query and format_url methods

--- sentinelsat/sentinel.py
+++ sentinelsat/sentinel.py
@@ -1,20 +1,40 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 
 
 def format_date(in_date):
+    """Format date or datetime input to YYYY-MM-DDThh:mm:ssZ"""
     return in_date.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
-class Query(object):
+class SentinelAPI(object):
+    """Class to connect to Sentinel-1 Scientific Data Hub, search and download
+    imagery.
+    """
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
 
-    def __init__(self, area, initial_date=None, end_date=datetime.now()):
+    def query(self, area, initial_date=None, end_date=datetime.now(), **keywords):
+        """Call the Scihub"""
+        self.format_url(area, initial_date, end_date, **keywords)
+        self.xml = requests.get(self.url, auth=(self.user, self.password))
+
+    def format_url(self, area, initial_date=None, end_date=datetime.now(), **keywords):
+        """Create the URL to access the SciHub API."""
+        if initial_date is None:
+            initial_date = end_date - timedelta(hours=24)
 
         ingestion_date = '(ingestionDate:[%s TO %s])' % (
             format_date(initial_date),
             format_date(end_date)
-            )
-        query_area = '(footprint:"Intersects(POLYGON((%s)))")' % area
-        self.url = 'https://scihub.esa.int/dhus/search?format=xml&q=%s AND %s' \
-            % (ingestion_date, query_area)
+        )
+        query_area = ' AND (footprint:"Intersects(POLYGON((%s)))")' % area
+
+        filters = ''
+        for kw in sorted(keywords.keys()):
+            filters += ' AND (%s:%s)' % (kw, keywords[kw])
+
+        self.url = 'https://scihub.esa.int/dhus/search?format=xml&q=%s%s%s' \
+            % (ingestion_date, query_area, filters)
--- tests/test_mod.py
+++ tests/test_mod.py
@@ -1,16 +1,35 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
+from os import environ
 
-from sentinelsat.sentinel import Query, format_date
+from sentinelsat.sentinel import SentinelAPI, format_date
 
 
 def test_format_date():
     assert format_date(datetime(2015, 1, 1)) == '2015-01-01T00:00:00Z'
     assert format_date(date(2015, 1, 1)) == '2015-01-01T00:00:00Z'
 
-def test_query():
-    area = '0 0,1 1,0 1,0 0'
-    query = Query(area, datetime(2015, 1, 1), datetime(2015, 1, 2))
-    expected_url = 'https://scihub.esa.int/dhus/search?format=xml' + \
+
+def test_SentinelAPI():
+    api = SentinelAPI(
+        environ.get('SENTINEL_USER'),
+        environ.get('SENTINEL_PASSWORD')
+        )
+    api.query('0 0,1 1,0 1,0 0', datetime(2015, 1, 1), datetime(2015, 1, 2))
+
+    assert api.url == 'https://scihub.esa.int/dhus/search?format=xml' + \
         '&q=(ingestionDate:[2015-01-01T00:00:00Z TO 2015-01-02T00:00:00Z]) ' + \
         'AND (footprint:"Intersects(POLYGON((0 0,1 1,0 1,0 0)))")'
-    assert query.url == expected_url
+    assert api.xml.status_code == 200
+
+    now = datetime.now()
+    api.format_url('0 0,1 1,0 1,0 0', end_date=now)
+    last_24h = format_date(now - timedelta(hours=24))
+    assert api.url == 'https://scihub.esa.int/dhus/search?format=xml' + \
+        '&q=(ingestionDate:[%s TO %s]) ' % (last_24h, format_date(now)) + \
+        'AND (footprint:"Intersects(POLYGON((0 0,1 1,0 1,0 0)))")'
+
+    api.format_url('0 0,1 1,0 1,0 0', end_date=now, producttype='SLC')
+    assert api.url == 'https://scihub.esa.int/dhus/search?format=xml' + \
+        '&q=(ingestionDate:[%s TO %s]) ' % (last_24h, format_date(now)) + \
+        'AND (footprint:"Intersects(POLYGON((0 0,1 1,0 1,0 0)))") ' + \
+        'AND (producttype:SLC)'
