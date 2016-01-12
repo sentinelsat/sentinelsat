5830c5b5a73636f99f07dcf86fa40f48fdaf7867
Fernerkundung <kersten.clauss@gmail.com>
2016-01-12 15:47:42 +0100

    Sentinel-2 parsing, tests, PEP8 corrections

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,36 +7,42 @@ from sentinelsat.scripts.cli import cli
 
 def test_cli():
     runner = CliRunner()
-    result = runner.invoke(cli,
+    result = runner.invoke(
+        cli,
         ['search',
         environ.get('SENTINEL_USER'),
         environ.get('SENTINEL_PASSWORD'),
-        'tests/map.geojson'
-        ])
+        'tests/map.geojson']
+        )
+
     assert result.exit_code == 0
 
-    result = runner.invoke(cli,
+    result = runner.invoke(
+        cli,
         ['search',
         environ.get('SENTINEL_USER'),
         environ.get('SENTINEL_PASSWORD'),
         'tests/map.geojson',
-        '--url', 'https://scihub.copernicus.eu/dhus/'
-        ])
+        '--url', 'https://scihub.copernicus.eu/dhus/']
+        )
     assert result.exit_code == 0
 
-    result = runner.invoke(cli,
+    result = runner.invoke(
+        cli,
         ['search',
         environ.get('SENTINEL_USER'),
         environ.get('SENTINEL_PASSWORD'),
         'tests/map.geojson',
-        '-q', 'producttype=GRD,polarisationmode=HH'
-        ])
+        '-q', 'producttype=GRD,polarisationmode=HH']
+        )
     assert result.exit_code == 0
+
 
 def test_returned_filesize():
     runner = CliRunner()
 
-    result = runner.invoke(cli,
+    result = runner.invoke(
+        cli,
         ['search',
         environ.get('SENTINEL_USER'),
         environ.get('SENTINEL_PASSWORD'),
@@ -44,12 +50,13 @@ def test_returned_filesize():
         '--url', 'https://scihub.copernicus.eu/dhus/',
         '-s', '20141205',
         '-e', '20141208',
-        '-q', 'producttype=GRD'
-        ])
+        '-q', 'producttype=GRD']
+        )
     expected = "1 scenes found with a total size of 0.50 GB"
     assert result.output.split("\n")[-2] == expected
 
-    result = runner.invoke(cli,
+    result = runner.invoke(
+        cli,
         ['search',
         environ.get('SENTINEL_USER'),
         environ.get('SENTINEL_PASSWORD'),
@@ -57,7 +64,7 @@ def test_returned_filesize():
         '--url', 'https://scihub.copernicus.eu/dhus/',
         '-s', '20140101',
         '-e', '20141231',
-        '-q', 'producttype=GRD'
-        ])
+        '-q', 'producttype=GRD']
+        )
     expected = "13 scenes found with a total size of 7.23 GB"
     assert result.output.split("\n")[-2] == expected
