3b554210af04043f517af783685cad128d4ddf0b
Kersten <kersten.clauss@gmail.com>
2016-04-08 14:02:05 +0200

    get_coordinate function

--- sentinelsat/sentinel.py
+++ sentinelsat/sentinel.py
@@ -314,7 +314,21 @@ class SentinelAPI(object):
 
 
 def get_coordinates(geojson_file, feature_number=0):
-    """Return the coordinates of a polygon of a GeoJSON file."""
+    """Return the coordinates of a polygon of a GeoJSON file.
+
+    Parameters
+    ----------
+    geojson_file : str
+        location of GeoJSON file_path
+    feature_number : int
+        Feature to extract polygon from (in case of MultiPolygon
+        FeatureCollection), defaults to first Feature
+
+    Returns
+    -------
+    polygon coordinates
+        string of comma separated coordinate tuples to be used by SentinelAPI
+    """
     geojson_obj = geojson.loads(open(geojson_file, 'r').read())
     coordinates = geojson_obj['features'][feature_number]['geometry']['coordinates'][0]
     # precision of 7 decimals equals 1mm at the equator
