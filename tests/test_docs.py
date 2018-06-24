3b0710acc8d5661d885c1dfba340c7a7c903c502
Martin Valgur <martin.valgur@gmail.com>
2018-06-24 22:22:18 +0300

    Fix a small rstcheck issue
    rstcheck did not recognize the 'automodule' directive

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -24,15 +24,21 @@ def test_rst(rst_file):
             'languages': ['python', 'bash']
         }
     )
-    for err_code, error in errors:
+    for line_number, error in errors:
         # report only warnings and higher, ignore Python and Bash pseudocode examples
         if 'Title underline too short' in error:
             # These are caused by unicode en dashes and can be ignored
             continue
+
         # Ignore to text roles provided via Sphinx extensions erroneously marked as unrecognized
         m = re.search('Unknown interpreted text role "([^"]+)"', error)
         if m and m.group(1) in ['program', 'paramref']:
             continue
-        all_errors.append(error)
+
+        m = re.search('Unknown directive type "([^"]+)"', error)
+        if m and m.group(1) in ['automodule']:
+            continue
+
+        all_errors.append((line_number, error))
 
     assert len(all_errors) == 0
