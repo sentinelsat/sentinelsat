ce174c97af17dcc559231ca340932f7c2f869335
Andrey Raspopov <9102888+Andrey-Raspopov@users.noreply.github.com>
2018-10-10 14:03:05 +0300

    Update sentinel.py
    Fix latitude boundaries

--- sentinelsat/sentinel.py
+++ sentinelsat/sentinel.py
@@ -847,7 +847,7 @@ def geojson_to_wkt(geojson_obj, feature_number=0, decimals=4):
         else:
             if geometry[0]>180 or geometry[0]<-180:
                 raise ValueError('Longitude is out of bounds, check your JSON format or data')
-            if geometry[1]>180 or geometry[1]<-180:
+            if geometry[1]>90 or geometry[1]<-90:
                 raise ValueError('Latitude is out of bounds, check your JSON format or data')
 
     # Discard z-coordinate, if it exists
