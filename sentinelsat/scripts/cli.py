eb4f0d0a30b15e54135394cecf2d84a820462835
Jonas Sølvsteen <j08lue@gmail.com>
2018-10-26 10:48:15 +0200

    Python 2 IOError

--- sentinelsat/scripts/cli.py
+++ sentinelsat/scripts/cli.py
@@ -32,7 +32,7 @@ def _try_netrc_auth(url):
     try:
         user, _, password = netrc.netrc(os.path.expanduser('~/.netrc')).authenticators(host)
         return user, password
-    except (FileNotFoundError, netrc.NetrcParseError, TypeError) as err:
+    except (IOError, netrc.NetrcParseError, TypeError) as err:
         logger.debug(err)
         print(err)
         return None, None
