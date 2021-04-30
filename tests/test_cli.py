1d9aac3245eb2f991e98f7dd35bb3a69103a69ec
Martin Valgur <martin.valgur@gmail.com>
2021-04-30 21:39:35 +0300

    update cassettes, fix trivial regressions
    - Kept test_info_cli cassette as is since the
    API Hub endpoint now correctly returns the DHuS version but other national hubs might be running older software.
    - Made some tests less dependent on specific products being returned.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -210,14 +210,14 @@ def test_cloud_flag_url(run_cli, geojson_path):
         "-s",
         "20151219",
         "-e",
-        "20151228",
+        "20151220",
         "-c",
         "10",
     ]
 
     result = run_cli("--sentinel", "2", *command)
 
-    expected = "Product 6ed0b7de-3435-43df-98bf-ad63c8d077ef - Date: 2015-12-27T14:22:29Z, Instrument: MSI, Mode: , Satellite: Sentinel-2, Size: 5.47 GB"
+    expected = "Product e071bdda-47ec-4434-aafa-00340442bdda - Date: 2015-12-19T14:47:22.029Z, Instrument: MSI, Satellite: Sentinel-2, Size: 403.60 MB"
     assert result.products[0] == expected
     # For order-by test
     assert "0848f6b8-5730-4759-850e-fc9945d42296" not in result.products[1]
@@ -236,15 +236,21 @@ def test_order_by_flag(run_cli, geojson_path):
         "-s",
         "20151219",
         "-e",
-        "20151228",
-        "-c",
-        "10",
-        "--sentinel",
-        "2",
+        "20151220",
         "--order-by",
-        "cloudcoverpercentage,-beginposition",
+        "platformname,-size",
     )
-    assert "0848f6b8-5730-4759-850e-fc9945d42296" in result.products[1]
+    # Check that order matches platformname,-size
+    sats = []
+    sizes = {}
+    for prod in result.products:
+        m = re.search(r"Satellite: (\S+), Size: (\S+ \S+)", prod)
+        assert m, prod
+        sats.append(m.group(1))
+        sizes.setdefault(m.group(1), []).append(m.group(2))
+    assert sats == sorted(sats)
+    for sizes_group in sizes.values():
+        assert sizes_group == sorted(sizes_group, reverse=True)
 
 
 @pytest.mark.vcr
@@ -281,21 +287,30 @@ def test_sentinel2_flag(run_cli, geojson_path):
         "20151228",
         "--sentinel",
         "2",
+        "--limit",
+        "5",
     )
-
-    expected = "Product 91c2503c-3c58-4a8c-a70b-207b128e6833 - Date: 2015-12-27T14:22:29Z, Instrument: MSI, Mode: , Satellite: Sentinel-2, Size: 5.73 GB"
-    assert expected in result.products
+    for prod in result.products:
+        assert "Satellite: Sentinel-2" in prod
 
 
 @pytest.mark.vcr
 @pytest.mark.scihub
 def test_sentinel3_flag(run_cli, geojson_path):
     result = run_cli(
-        "--geometry", geojson_path, "-s", "20161201", "-e", "20161202", "--sentinel", "3"
+        "--geometry",
+        geojson_path,
+        "-s",
+        "20161201",
+        "-e",
+        "20161202",
+        "--sentinel",
+        "3",
+        "--limit",
+        "5",
     )
-
-    expected = "Product 1d16f909-de53-44b0-88ad-841b0cae5cbe - Date: 2016-12-01T13:12:45.561Z, Instrument: SRAL, Mode: , Satellite: Sentinel-3, Size: 2.34 GB"
-    assert expected in result.products
+    for prod in result.products:
+        assert "Satellite: Sentinel-3" in prod
 
 
 @pytest.mark.vcr
@@ -324,9 +339,9 @@ def test_instrument_flag(run_cli, geojson_path):
     result = run_cli(
         "--geometry", geojson_path, "-s", "20161201", "-e", "20161202", "--instrument", "SRAL"
     )
-
-    expected = "Product 1d16f909-de53-44b0-88ad-841b0cae5cbe - Date: 2016-12-01T13:12:45.561Z, Instrument: SRAL, Mode: , Satellite: Sentinel-3, Size: 2.34 GB"
-    assert expected in result.products
+    for prod in result.products:
+        assert "Instrument: SRAL" in prod
+        assert "Date: 2016-12-01" in prod or "Date: 2016-12-02" in prod
 
 
 @pytest.mark.vcr
@@ -354,7 +369,7 @@ def test_limit_flag(run_cli, geojson_path):
 def test_uuid_search(run_cli):
     result = run_cli("--uuid", "d8340134-878f-4891-ba4f-4df54f1e3ab4")
 
-    expected = "Product d8340134-878f-4891-ba4f-4df54f1e3ab4 - S1A_WV_OCN__2SSV_20150526T211029_20150526T211737_006097_007E78_134A - 0.12 MB"
+    expected = "Product d8340134-878f-4891-ba4f-4df54f1e3ab4 - S1A_WV_OCN__2SSV_20150526T211029_20150526T211737_006097_007E78_134A - 0.2 MB"
     assert result.products[0] == expected
 
 
@@ -413,7 +428,7 @@ def test_option_hierarchy(run_cli, geojson_path):
 
     # Check that all returned products are of type 'S2MSI1C'
     assert len(result.products) > 0
-    assert all("Instrument: MSI, Mode: , Satellite: Sentinel-2" in p for p in result.products)
+    assert all("Instrument: MSI, Satellite: Sentinel-2" in p for p in result.products)
 
 
 @pytest.mark.vcr
