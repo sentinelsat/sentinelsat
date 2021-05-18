797c2171fb5fb7218586d022cfdcd45df68582cd
Martin Valgur <martin.valgur@gmail.com>
2021-05-18 14:13:45 +0300

    mark cancelled products as failed

--- sentinelsat/sentinel.py
+++ sentinelsat/sentinel.py
@@ -802,12 +802,11 @@ class SentinelAPI:
 
         failed_prods = {}
         for pid, info in product_infos.items():
-            if (
-                pid not in downloaded_prods
-                and pid not in retrieval_scheduled
-                and not dl_tasks[pid].cancelled()
-            ):
-                info["exception"] = dl_tasks[pid].exception()
+            if pid not in downloaded_prods and pid not in retrieval_scheduled:
+                if dl_tasks[pid].cancelled():
+                    info["exception"] = concurrent.futures.CancelledError()
+                else:
+                    info["exception"] = dl_tasks[pid].exception()
                 failed_prods[pid] = info
 
         if len(failed_prods) == len(product_ids) and len(product_ids) > 0:
